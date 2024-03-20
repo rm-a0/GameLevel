e900e3f178cc06a7a9058c3e67717a57d1dd4247
Michal Repčík <148249444+rm-a0@users.noreply.github.com>
2024-03-20 09:24:00 +0100

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,21 @@
+#window resolution
+WIDTH = 1536
+HEIGHT = 864
+
+#dont change fps, the game wont work as intended (applyGravity, movement and refillStamina will not work properly)
+FPS = 120
+DELAY = int(1000/FPS)
+
+GRAVITY = 1000
+
+RIGHT = 0
+LEFT = 1
+
+DAMAGE = 0
+POSION = 1
+SCARLET_ROT = 2
+BLOOD_LOSS = 3
+FROSTBITE = 4
+SLEEP = 5
+MADNESS = 6
+DEATH_BLIGHT = 7
