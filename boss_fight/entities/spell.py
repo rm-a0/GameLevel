f9c0b6e567e38102332a1dea18bf7f075ce3b9a8
Michal Repčík <148249444+rm-a0@users.noreply.github.com>
2024-03-20 09:25:01 +0100

    Delete boss_fight directory

--- boss_fight/entities/spell.py
+++
@@ -1,7 +0,0 @@
-class Spell:
-    def __init__(self, canvas, name, effect, magnitude, manaCost, castDuration):
-        self.name = name
-        self.effect = effect
-        self.magnitude = magnitude
-        self.manaCost = manaCost
-        self.castDuratio = castDuration
